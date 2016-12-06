40fd665d14b1e492cefede0a3864e52c51f9a441
bryan.yang <bryan.yang@ringcentral.com>
2016-12-06 06:47:52 +0000

    replace test data, add test for anova

--- pga/test/test.py
+++ pga/test/test.py
@@ -20,33 +20,37 @@ from sklearn.utils.testing import assert_raise_message
 from pga.pga import PeerGroup
 
 df = pd.read_csv('data/sample_data.csv', sep = ',', header=0)
-pg = PeerGroup(df, n_clusters=10, random_state=0, n_init=1)
+df_pg = df[['id', 'value']]
+pg = PeerGroup(df_pg, n_clusters=10, random_state=0, n_init=1)
 
 def test_data_len():
     assert_equal(len(df), 100)
 
 def test_describe():
-    desc_array = np.array([[  6. ,  10. ,  95.5],
-                          [  1. ,  11. ,  85. ],
-                          [  7. ,  12. ,  73.5],
-                          [  3. ,  14. ,  60.5],
-                          [  0. ,  13. ,  47. ],
-                          [  4. ,  11. ,  35. ],
-                          [  9. ,   9. ,  25. ],
-                          [  2. ,   7. ,  17. ],
-                          [  8. ,   6. ,  10.5],
-                          [  5. ,   7. ,   4. ]])
+    desc_array = np.array([[   8.        ,    7.        ,  964.57142857],
+                           [   0.        ,    8.        ,  872.625     ],
+                           [   2.        ,   11.        ,  762.90909091],
+                           [   9.        ,   13.        ,  683.30769231],
+                           [   5.        ,    8.        ,  610.5       ],
+                           [   4.        ,    7.        ,  508.71428571],
+                           [   6.        ,   12.        ,  402.91666667],
+                           [   1.        ,   11.        ,  291.54545455],
+                           [   7.        ,   13.        ,  149.92307692],
+                           [   3.        ,   10.        ,   54.6       ]])
     pg_desc = pg.describe()
-    assert_array_equal(pg_desc.values, desc_array)
+    assert_array_almost_equal(pg_desc.values, desc_array)
 
 def test_head():
     assert_array_equal(pg.head().values, pg.get_data().head().values)
 
 def test_get_data():
-    head_array = np.array([[ 1.,  5.,  1.],
-                           [ 1.,  5.,  2.],
-                           [ 1.,  5.,  3.],
-                           [ 1.,  5.,  4.],
-                           [ 1.,  5.,  5.]])
+    head_array = np.array([[   1.,    7.,  120.],
+                           [   2.,    6.,  362.],
+                           [   3.,    7.,  185.],
+                           [   4.,    9.,  701.],
+                           [   5.,    8.,  940.]])
 
-    assert_array_equal(pg.get_data().head().values, head_array)
+    assert_array_almost_equal(pg.get_data().head().values, head_array)
+
+def test_anova():
+    assert_almost_equal(pg.anova()['PR(>F)'][0], 0.68092934459725607)
